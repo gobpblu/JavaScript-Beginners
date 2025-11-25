d077f131c5d86751330dec4d3172b91bc4fb0a26
Abramov Max <gobpo2002@gmail.com>
2025-11-25 10:25:48 +0600

    lesson 2.3

---
+++ chapter2_JS_variables/lesson3_primitive_types.js
@@ -0,0 +1,24 @@
+let favouriteFruit = 'banana';
+let favoriteIceCream = 'vanilla';
+let favoriteProgrammingLanguage = 'Kotlin';
+
+let numberOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNumber = 545435436436543n;
+
+let lovesCoding = true;
+
+let favoriteColor;
+console.log(favoriteColor);
+
+favoriteFruit = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+    name: 'JavaScript for Beginners',
+    hours: 3
+};
+
+console.log(course);
