b78c87895cd69ea17ea494cc29b3b31815e30033
Abramov Max <gobpo2002@gmail.com>
2025-11-25 17:36:47 +0600

    lesson 3.5

---
+++ chapter3_operators/lesson5_ternary_operators.js
@@ -0,0 +1,7 @@
+let age = 16;
+const canDrive = age >= 16 ? true : false;
+console.log(canDrive);
+
+let points = 110;
+const customerType = points > 100 ? 'Gold' : 'Silver';
+console.log(customerType);
