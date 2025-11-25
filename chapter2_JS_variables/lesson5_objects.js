44d27c6e9ef0752529024411872eae9b6ab830fd
Abramov Max <gobpo2002@gmail.com>
2025-11-25 14:55:12 +0600

    lesson 2.5

---
+++ chapter2_JS_variables/lesson5_objects.js
@@ -0,0 +1,16 @@
+let course = {
+    name: 'JavaScript for Beginners',
+    hours: 3
+};
+
+console.log(course.name);
+course.name = 'JavaScript Fundamentals';
+console.log(course.name);
+console.log(course['name']);
+
+course['name'] = 'JavaScript 1.1';
+console.log(course['name']);
+
+let property = 'hours';
+
+console.log(course[property]);
