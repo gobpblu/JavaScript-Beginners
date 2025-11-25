a59fcf8d9eb85954f97a254b02f3b761730aaa4e
Abramov Max <gobpo2002@gmail.com>
2025-11-25 15:23:12 +0600

    lesson 3.1

---
+++ chapter3_operators/lesson1_arithmetic_operators.js
@@ -0,0 +1,21 @@
+/* -, +, *, /, %, ** +=, -=, *=, /=, ++, -- */
+let a = 5;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+a += 5;
+console.log(a);
+a -= 5;
+console.log(a);
+a *= 5;
+console.log(a);
+a /= 5;
+console.log(a);
+a++;
+console.log(a);
+a--;
+console.log(a);
