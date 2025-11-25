e4a902c89571e20ce087488e43ee107a470ca1b9
Abramov Max <gobpo2002@gmail.com>
2025-11-25 15:58:30 +0600

    lesson 3.4

---
+++ chapter3_operators/lesson3_comparison_operators.js
@@ -0,0 +1,14 @@
+let num1 = 14;
+let num2 = 10;
+
+const isNum1Greater = num1 > num2;
+console.log(isNum1Greater);
+
+const isNum1GreaterOrEqual = num1 >= num2;
+console.log(isNum1GreaterOrEqual);
+
+const isNum1LessThan = num1 < num2;
+console.log(isNum1LessThan);
+
+const isNum1LessThanOrEqual = num1 <= num2;
+console.log(isNum1LessThanOrEqual);
